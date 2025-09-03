27b4bbde1da835588792e38c3197603958a30944
Hetti219 <sathikahettiarachchi219@gmail.com>
2025-09-03 18:19:01 +0530

    Update permissions and build config

--- android/app/src/main/kotlin/io/github/hetti219/fulminant_c/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/hetti219/fulminant_c/MainActivity.kt
@@ -1,5 +1,5 @@
 package io.github.hetti219.fulminant_c
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
